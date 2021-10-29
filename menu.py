97db70b43c863c04df448d5fead8b5cf5efa74f4
ElcimarSilva <elcimar8@gmail.com>
2021-10-29 00:03:20 -0300

    primeiro commit projeto flask

---
+++ menu.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template #render serve parar puxar as pags html
+
+app = Flask(__name__)  # Convenção do flask passar esta variavel
+
+@app.route("/") # nome do site "app" + a rota que vai ser chamada
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/contatos")
+def contatos():
+    return render_template("contatos.html")
+
+if __name__ == "__main__": # Executa o codigo somente se o arquivo que foi executado for o main
+    app.run(debug=True)  # Executa o site
---
+++ readme.md
@@ -0,0 +1,8 @@
+Video iniciando sobre o Flask na hashtag
+https://www.youtube.com/watch?v=K2ejI4z8Mbg
+Tempo de video 30:00
+
+
+requisitos
+Estar dentro da venv 
+pip install Flask
